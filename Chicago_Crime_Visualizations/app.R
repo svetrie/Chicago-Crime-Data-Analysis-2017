a9887c5ea3dc50b2e0ce6ac95dab12d94bc8403f
Vetrie Senthilkumar <vetries2@illinois.edu>
2018-05-08 02:04:47 -0500

    Added color to bar graphs and added app feature that only displays map markers for selected crime type

--- Chicago_Crime_Visualizations/app.R
+++ Chicago_Crime_Visualizations/app.R
@@ -30,14 +30,14 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("Chicago Crime Data Visualizations"),
-  
-   leafletOutput("crime_map"),
    
    selectInput(inputId = "crime_type",
                label = "Select Type of Crime",
-               choices = as.character(unique(crime_dataset$Primary.Type)),
-               selected = "THEFT"
+               choices = c("ALL", as.character(unique(crime_dataset$Primary.Type))),
+               selected = "ALL"
    ),
+  
+   leafletOutput("crime_map"),
    
    plotlyOutput("crime_rate_plot"),
    
@@ -52,17 +52,28 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+
+  crime_type_subset = reactive({
+    if(input$crime_type == "ALL") {
+      crime_dataset
+    } else {
+      subset(crime_dataset, crime_dataset$Primary.Type == input$crime_type)
+    }
+  })
+  
+  crime_rate = reactive({
+    count(crime_type_subset(), Date)
+  })
+  
   output$crime_map = renderLeaflet(leaflet() %>%
                                      addTiles() %>%
-                                     addCircleMarkers(lng = crime_dataset$Longitude, lat = crime_dataset$Latitude,
-                                                      popup = paste("Crime:", crime_dataset$Primary.Type, "<br>",
-                                                                    "Date:", as.character.Date(crime_dataset$Date,
+                                     addCircleMarkers(lng = crime_type_subset()$Longitude, lat = crime_type_subset()$Latitude,
+                                                      popup = paste("Crime:", crime_type_subset()$Primary.Type, "<br>",
+                                                                    "Date:", as.character.Date(crime_type_subset()$Date,
                                                                                                format = "%m/%d/%y"), "<br>",
-                                                                    "Arrest: ", crime_dataset$Arrest),
-                                                      radius = 1, fill = TRUE))
+                                                                    "Arrest: ", crime_type_subset()$Arrest),
+                                                      radius = 1, fill = TRUE, color = "#f02ddc"))
   
-  crime_rate = reactive(count(subset(crime_dataset,
-                                     crime_dataset$Primary.Type == input$crime_type), Date))
 
   output$crime_rate_plot = renderPlotly(ggplotly(ggplot(crime_rate(), aes(x = Date, y = n)) +
                                                 geom_line(colour = "turquoise", size = .8) +
@@ -70,8 +81,7 @@ server <- function(input, output) {
                                                 scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
                                                 theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
                                                 theme(legend.position = "none") +
-                                                labs(title = "Overall Crime Rate",
-                                                     subtitle = as.character(input$crime_type),x = "Date", y = "Number of Assaults"),
+                                                labs(title = "Overall Crime Rate", x = "Date", y = "Number of Crime Incidents"),
                                                 tooltip = "text")
   
                                          )
@@ -79,14 +89,14 @@ server <- function(input, output) {
   district_subset = reactive(subset(crime_dataset, crime_dataset$District == input$district_slider))
   
   output$district_crime_plot = renderPlotly(ggplotly(ggplot(district_subset(), aes(x = Primary.Type)) + 
-                                                       geom_bar() + 
+                                                       geom_bar(aes(fill = ..count..)) + 
                                                        labs(title = "Crime Rates for Different Types of Crimes in District",
                                                             x = "Types of Crimes", y = "Number of Occurrences") +
-                                                       theme(axis.text.x = element_text(angle = 90, hjust = 1))
+                                                       theme(axis.text.x = element_text(angle = 90, hjust = 1),
+                                                             legend.key.height = unit(3, "inches"))  + 
+                                                       scale_fill_gradient(low = "#ffe905", high = "#f51f00")
                                                      )
                                             )
-  
-  
 }
 
 
